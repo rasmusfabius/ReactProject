fcd358fd926fd3f3b59e02108e6c059631184eb7
rasmusfabius <rasmusfabius@gmail.com>
2019-11-24 15:30:16 +0100

    Adding spinner and Tomanyrequets badge

--- pokemondexapi/src/components/pokemon/PokemonList.js
+++ pokemondexapi/src/components/pokemon/PokemonList.js
@@ -4,7 +4,7 @@ import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
   state = {
-    url: 'https://pokeapi.co/api/v2/pokemon/',
+    url: 'https://pokeapi.co/api/v2/pokemon?limit=500',
     pokemon: null
   };
 
